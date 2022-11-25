dcbbd4bf5f71e83af34c6e7f52f20070e37ca562
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-11-25 10:59:13 +0100

    fix ld

--- flow.go
+++ flow.go
@@ -14,13 +14,15 @@ func init() {
 }
 
 var version string
+var foo string
+var boo string
 
 func main() {
 
 	APIURL := viper.GetString("APIURL")
 
 	gateway := gateway.CreateGateway(models.GRPC, APIURL)
-	rootCmd := cmd.NewCmdRoot(gateway, version)
+	rootCmd := cmd.NewCmdRoot(gateway, version+foo+boo)
 
 	if err := rootCmd.Execute(); err != nil {
 		util.HandleError(err)
